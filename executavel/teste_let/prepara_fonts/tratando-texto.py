785751526ce916f83e4fb685d8444c06ed0b3704
RODRIGO NASCIMENTO DOS SANTOS <rodrigo.santos@smsregulacao.realtecprojetos.com.br>
2026-07-23 19:41:33 -0300

    arquivo para salvar pct

--- executavel/teste_let/prepara_fonts/tratando-texto.py
+++ executavel/teste_let/prepara_fonts/tratando-texto.py
@@ -5,8 +5,39 @@ root2 = tk.Tk()
 
 root2.title('New note block')
 
-tk.Label(root2, text='insira as informações para inserir na planilha').pack()
-entrada_texto = ttk.Entry(root2, width=50)
-entrada_texto.pack(ipadx=20, ipady=30)
+pct = ttk.Label(root2, text='Insira as informações do atendimento')
+pct.pack()
+
+frame = ttk.Frame(root2)
+frame.pack(padx=10, pady=10, fill=tk.BOTH, expand=True)
+barra_rolagem = ttk.Scrollbar(frame)
+barra_rolagem.pack(side=tk.RIGHT, fill=tk.Y)
+
+texto = tk.Text(frame, height=20)
+texto.pack(side=tk.LEFT, expand=True, fill=tk.BOTH)
+texto['yscrollcommand']=barra_rolagem.set
+
+barra_rolagem.config(command=texto.yview)
+
+texto.insert(tk.END, "\n")
+
+def janela_confirmacao():
+   #antes de abrir a janela salvar o arquivo
+   #criar variaveis e loop para enquanto o usuario disser "sim" o programa salvar o nome do programa{numero do pct}
+   tela_f = tk.Tk()
+
+   tela_f.title('confirmação')
+
+   mensagem = ttk.Label(tela_f, text='Vai inserir mais algum pct na planilha?')
+   mensagem.pack()
+
+   
+
+   tela_f.mainloop()
+
+enviar = ttk.Button(root2, text='Salvar paciente', command=janela_confirmacao)
+enviar.pack()
+
+
 
 root2.mainloop()
